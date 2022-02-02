8b22b3a493b9150693d7dbb70b40ba9494b655eb
sistr <game.sistr@gmail.com>
2022-02-02 13:45:18 +0900

    caps追加

--- common/src/main/java/net/sistr/littlemaidrebirth/entity/LittleMaidModelCaps.java
+++ common/src/main/java/net/sistr/littlemaidrebirth/entity/LittleMaidModelCaps.java
@@ -1,6 +1,9 @@
 package net.sistr.littlemaidrebirth.entity;
 
+import com.google.common.collect.ImmutableSet;
+import net.minecraft.item.Items;
 import net.sistr.littlemaidmodelloader.maidmodel.EntityCaps;
+import net.sistr.littlemaidrebirth.api.mode.Mode;
 
 public class LittleMaidModelCaps extends EntityCaps {
     private final LittleMaidEntity maid;
@@ -10,7 +13,7 @@ public class LittleMaidModelCaps extends EntityCaps {
         this.maid = maid;
     }
 
-    //todo 色々追加
+    //todo インベントリ系、トレーサー
     @Override
     public Object getCapsValue(int pIndex, Object... pArg) {
         switch (pIndex) {
@@ -19,7 +22,22 @@ public class LittleMaidModelCaps extends EntityCaps {
             case caps_isLookSuger:
                 return maid.isBegging();
             case caps_interestedAngle:
-                return maid.getInterestedAngle((Float)pArg[0]);
+                return maid.getInterestedAngle((Float) pArg[0]);
+            case caps_isFreedom:
+                return maid.getMovingState() == Tameable.MovingState.FREEDOM;
+            case caps_isContract:
+                return maid.isContract();
+            case caps_isClock:
+                return maid.getMainHandStack().getItem() == Items.CLOCK
+                        || maid.getOffHandStack().getItem() == Items.CLOCK
+                        || maid.getInventory().containsAny(ImmutableSet.of(Items.CLOCK));
+            case caps_job:
+                return maid.getMode()
+                        .map(Mode::getName)
+                        .map(String::toLowerCase)
+                        .orElse(null);
+            case caps_isLeeding:
+                return maid.isLeashed();
         }
         return super.getCapsValue(pIndex, pArg);
     }
