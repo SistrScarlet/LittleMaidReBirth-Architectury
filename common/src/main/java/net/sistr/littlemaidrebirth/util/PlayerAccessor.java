d2fc339b8effefbb73024d2520e76547c88e8c14
SistrScarlet <game.sistr@gmail.com>
2022-04-08 15:29:11 +0900

    まだForgeで起動できなかったので、accesswidenerを使わないように変更

--- common/src/main/java/net/sistr/littlemaidrebirth/util/PlayerAccessor.java
+++ common/src/main/java/net/sistr/littlemaidrebirth/util/PlayerAccessor.java
@@ -1,9 +1,12 @@
 package net.sistr.littlemaidrebirth.util;
 
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.PlayerInventory;
 
 public interface PlayerAccessor {
 
     void onCollideWithEntity_LM(Entity entity);
+
+    void setInventory(PlayerInventory inventory);
 
 }
