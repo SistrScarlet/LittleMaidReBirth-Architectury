89cd03e1ff48c29146d76a424692764d8a1f0ff2
sistr <game.sistr@gmail.com>
2022-07-20 22:02:53 +0900

    リード付くように&MaidModelRendererをMobEntityRendererに変更

--- common/src/main/java/net/sistr/littlemaidrebirth/entity/LittleMaidEntity.java
+++ common/src/main/java/net/sistr/littlemaidrebirth/entity/LittleMaidEntity.java
@@ -23,10 +23,7 @@ import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.entity.projectile.thrown.SnowballEntity;
 import net.minecraft.inventory.Inventory;
 import net.minecraft.inventory.StackReference;
-import net.minecraft.item.ItemStack;
-import net.minecraft.item.ItemUsage;
-import net.minecraft.item.Items;
-import net.minecraft.item.RangedWeaponItem;
+import net.minecraft.item.*;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.nbt.NbtHelper;
 import net.minecraft.network.Packet;
@@ -760,6 +757,11 @@ public class LittleMaidEntity extends TameableEntity implements CustomPacketEnti
         return -fallDamage;
     }
 
+    @Override
+    public Vec3d getLeashOffset() {
+        return new Vec3d(0.0, this.getStandingEyeHeight() - 0.15f, 1f / 16f);
+    }
+
     //todo 以下数メソッドにはもうちと整理が必要か
 
     //trueでアイテムが使用された、falseでされなかった
@@ -774,7 +776,7 @@ public class LittleMaidEntity extends TameableEntity implements CustomPacketEnti
             player.setStackInHand(hand, itemStack2);
             return ActionResult.success(this.world.isClient);
         }
-        if (player.isSneaking() || stack.getItem() instanceof IFFCopyBookItem) {
+        if (player.isSneaking() || stack.getItem() instanceof IFFCopyBookItem || stack.getItem() instanceof LeadItem) {
             return ActionResult.PASS;
         }
         if (!hasTameOwner()) {
